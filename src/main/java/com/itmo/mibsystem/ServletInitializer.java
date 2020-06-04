08810a0285aedd404bf51a2c2390bb8b6e26bc09
kbalad <kirillbalad@gmail.com>
2020-06-04 23:38:58 +0300

    add initial context logic

--- src/main/java/com/itmo/mibsystem/ServletInitializer.java
+++ src/main/java/com/itmo/mibsystem/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.itmo.mibsystem;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * The class allows to deploy app as WAR
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
