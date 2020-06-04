847642822e4f8f2b4a88c1d3fb420bfe2e859728
kbalad <kirillbalad@gmail.com>
2020-06-04 17:57:32 +0300

    init commit

---
+++ src/main/java/com/itmo/mibsystem/MibSystemApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.mibsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MibSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MibSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itmo/mibsystem/MibSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.mibsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MibSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
